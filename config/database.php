0b6d75b373f5196c38f7330223896f7f0e6a8fb5
Antho <anthony.34.lopez@gmail.com>
2022-06-24 15:25:03 +0200

    db config

--- config/database.php
+++ config/database.php
@@ -89,7 +89,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'gdhnawra_landlord_db',
+            'database' => 'gdhnawra_landlord_cms',
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
